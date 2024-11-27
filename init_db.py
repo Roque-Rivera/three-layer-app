495d80abcea07428e61e5dc1b821c22ceef00c5b
Roque Rivera <roque.rivera@gmail.com>
2024-11-27 13:25:32 +0100

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,20 +1,21 @@
-from app import db, Item
+from app import db, Item, app
 from dotenv import load_dotenv
 
 def init_database():
-    db.create_all()
-    
-    # Add some sample items
-    sample_items = [
-        Item(name='Item 1', description='First sample item'),
-        Item(name='Item 2', description='Second sample item'),
-        Item(name='Item 3', description='Third sample item')
-    ]
-    
-    for item in sample_items:
-        db.session.add(item)
-    
-    db.session.commit()
+    with app.app_context():
+        db.create_all()
+        
+        # Add some sample items
+        sample_items = [
+            Item(name='Item 1', description='First sample item'),
+            Item(name='Item 2', description='Second sample item'),
+            Item(name='Item 3', description='Third sample item')
+        ]
+        
+        for item in sample_items:
+            db.session.add(item)
+        
+        db.session.commit()
 
 if __name__ == '__main__':
     load_dotenv()
